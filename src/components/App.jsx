18210ca331f2ce4f90efa8adeb96b66b29289066
Vovcharenko Anna <129333985+AnnaVovcharenko@users.noreply.github.com>
2023-10-23 00:47:11 +0300

    end

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,87 +3,84 @@ import { SearchBar } from './Searchbar/Searchbar';
 import { Button } from './Button/Button';
 import * as API from '../api/Api';
 
-import { Component } from 'react';
+import {useState , useEffect } from 'react';
 import {Loader} from './Loader/Loader';
 
 import { ToastContainer, toast, Slide } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-export class App extends Component {
- // всі коменти які я роблю, були взяті з документації React та конспекту 
-//ЗМІНА НАЗВ ВЛАСТИВОСТЕЙ КОМПОНЕНТІВ(збереження даних компонентів)
-state = {
-  query: "", 
-  images: [],
-  error: false,
-  loading: false, 
-  page: 1,
-  totalImges: 0 
-}
+export const App = () =>  {
 
-  
-  // Стадій життєвого циклу – монтування (Викликається відразу після монтування компонента в DOM)
-  async componentDidUpdate(_, prevState) {
-    // Обгортаєм виклик в умову, щоб уникнути безкінечного циклу
-    if (
-      prevState.query !== this.state.query || //запит
-      prevState.page !== this.state.page //номер сторінки
-    ) { this.setState({ loading: true });
-       // Отримання та додавання зображень
-       try{ 
-        const data = await API.fetchImg(this.state.query, this.state.page);
-        if (data.hits.length === 0) {
-          //
-          return toast.info('Sorry image not found...', {
-            position: toast.POSITION.TOP_RIGHT,
-          });
-        } 
-        const normalizedImg = API.normalizedImg(data.hits);
-        this.setState(state => ({
-          images: [...state.images, ...normalizedImg], // Додаємо нові зображення до існуючих
-          loading: false, // Прибираємо індикатор завантаження зображень
-          error: '', // Очищаємо повідомлення про помилку
-          totalImges: Math.ceil(data.totalHits / 12), //Вираховуємо  загальну кількість сторінок
-        }));
-      } catch (error) {
-        this.setState({ error: 'Something went wrong!' }); // якщо виникла помилка
-      } finally {
-        this.setState({  loading: false }); //Прибираємо індикатор завантаження зображень в будь-якому випадку
-      }};
+const [query, setQuery] = useState('');
+const [images, setImages] = useState([]);
+const [error, setError] = useState(false);
+const [loading, setLoading] = useState(false);
+const [page, setPage] = useState(1);
+const [totalImges, setTotalImges] = useState(0);
+
+useEffect(() => {
+  if (query === '') {
+    return;
   }
-    
+  async function addImg () {
+    try{ 
+      setLoading(true);
+    const data = await API.fetchImg(query, page);
+
+    if (data.hits.length === 0) {
+      
+      return toast.info('Sorry image not found...', {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    } 
+    const normalizedImg = API.normalizedImg(data.hits);
+
+    setImages(prevImages => [...prevImages, ...normalizedImg], // Додаємо нові зображення до існуючих
+    setLoading(false), // Прибираємо індикатор завантаження зображень
+    setError(''), // Очищаємо повідомлення про помилку
+    setTotalImges(Math.ceil(data.totalHits / 12)), //Вираховуємо  загальну кількість сторінок
+    );
+  } catch (error) {
+    toast.error('Something went wrong!'); // якщо виникла помилка
+  } finally {
+    setLoading(false); //Прибираємо індикатор завантаження зображень в будь-якому випадку
+  }
+}
+addImg ();
+}, [query, page]);
+  
+  
   
   //Оброблення та відправлення форми
-  handledSubmit = value => {
-    this.setState({
-      query: value, //Встановлюємо введений запит до стану
-      images: [], //Очищаємо масив із зображеннями
-      page: 1, //Скидаємо номер поточної сторінки на першу
-    });
+  const handledSubmit = value => {
+    
+    setQuery(value); //Встановлюємо введений запит до стану
+    setImages([]); //Очищаємо масив із зображеннями
+    setPage(1); //Скидаємо номер поточної сторінки на першу
+    
   };
   //Завантаження додаткових зображень шляхом збільшення номера поточної сторінки
-  nextPortionImg = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
+ const  nextPortionImg = () => {
+    
+      setPage( prevPage => prevPage + 1,)
+    
   };
 
-  render() {
-    const { images,  page, totalImges, loading, } = this.state;
+  
     return (
       <div>
         <ToastContainer transition={Slide} />
-        <SearchBar onSubmit={this.handledSubmit} />
+        <SearchBar onSubmit={handledSubmit} />
         <ImageGallery 
         images={images} />
 
         {loading && <Loader />}
         {totalImges > 1 && page < totalImges && (
-          <Button onClick={this.nextPortionImg} /> // Кнопка для завантаження додаткових зображень
+          <Button onClick={nextPortionImg} /> // Кнопка для завантаження додаткових зображень
         )}
                
        
       </div>
     );
-  }
+  
 }
--- src/components/ImageGalleryItem/ImageGalleryItem.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.jsx
@@ -1,20 +1,20 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { ModalWindow } from '../Modal/Modal';
 import { Item, Img } from './ImageGalleryItem.styled';
-export class ImgGalleryItem extends Component {
-  state = {
-    isModalOpen: false, //Зберігає стан модального вікна (відчинено чи закрито)
+
+
+export const ImgGalleryItem = ({image} ) => {
+ const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const openModal = () => {
+    setIsModalOpen(true);
+    // this.setState({ isModalOpen: true }); // відкриття модального вікна
   };
-  openModal = () => {
-    this.setState({ isModalOpen: true }); // відкриття модального вікна
-  };
-  closeModal = () => {
-    this.setState({ isModalOpen: false }); // закриття модального вікна
+  const closeModal = () => {
+    setIsModalOpen(false);
+    // this.setState({ isModalOpen: false }); // закриття модального вікна
   };
 
-  render() {
-    const { isModalOpen } = this.state; //Отримуємо поточне значення isModalOpen зі стану
-    const { image } = this.props; // Отримуємо переданий пропс
     
     return (
       <>
@@ -22,12 +22,12 @@ export class ImgGalleryItem extends Component {
           <Img
             src={image.webformatURL}
             alt={image.tags}
-            onClick={this.openModal}
+            onClick={openModal}
           />
           <ModalWindow
             isOpen={isModalOpen}
             contentLabel="Example Modal"
-            onRequestClose={this.closeModal}
+            onRequestClose={closeModal}
             largeImageURL={image.largeImageURL}
           tags={image.tags}
           >  
@@ -36,5 +36,5 @@ export class ImgGalleryItem extends Component {
         </Item>
       </>
     );
-  }
+  
 }
